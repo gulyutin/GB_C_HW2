8f795e68f348f5d15538e28d6b5f59d4501a3744
Gulyutin <gulyutin@gmail.com>
2022-07-28 17:12:48 +0700

    Example 3 solved

---
+++ EX3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 
+// 14 -> нет 46 -> нет 161 -> да
+
+Console.Write("Введите число ");
+string firstString = Console.ReadLine();
+int firstArg = Convert.ToInt32(firstString);
+
+if (firstArg % 7 == 0 && firstArg % 23 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+        else 
+        {
+            Console.Write("Нет");
+        }
+
