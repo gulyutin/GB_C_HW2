5fec4ad3627cf9157d51436090502a5791f16933
Gulyutin <gulyutin@gmail.com>
2022-07-28 16:57:36 +0700

    Comment added

--- EX1/Program.cs
+++ EX1/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введите трехзначное число: ");
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 
+// 456 -> 46 782 -> 72 918 -> 98
+
+Console.Write("Введите трехзначное число: ");
 string firstString = Console.ReadLine();
 int firstArg = Convert.ToInt32(firstString);
 int SecondNumber = 0;
