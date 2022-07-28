dfa0d838f17aca1a955a8a7937be7e20614675e3
Gulyutin <gulyutin@gmail.com>
2022-07-28 17:06:52 +0700

    Example 2 solved

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,18 +1,20 @@
 ﻿// Напишите программу, которая выводит случайное трёхзначное число 
 // и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
 
-Console.Write("Введите первое число");
+Console.Write("Введите первое число ");
 string firstString = Console.ReadLine();
 int firstArg = Convert.ToInt32(firstString);
-Console.Write("Введите второе число");
+Console.Write("Введите второе число ");
 string secondString = Console.ReadLine();
 int secondArg = Convert.ToInt32(secondString);
-int Div = firstArg %% secondArg 
+int Div = firstArg % secondArg; 
 
-if (Div == 0 )
+if (Div == 0)
     {
         Console.WriteLine("Кратно ");
     }
         else 
-        Console.WriteLine("Не кратно, остаток ");
+        {
+        Console.Write("Не кратно, остаток ");
         Console.Write(Div);
+        }
