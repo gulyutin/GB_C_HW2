abc211b13ecab3a53dd59a4107139db64a9a61ef
Gulyutin <gulyutin@gmail.com>
2022-07-28 16:56:15 +0700

    Example 1 resolved

--- EX1/Program.cs
+++ EX1/Program.cs
@@ -1,9 +1,12 @@
-﻿Console.WriteLine("Введите трехзначное число.");
-    while(true)
+﻿Console.Write("Введите трехзначное число: ");
+string firstString = Console.ReadLine();
+int firstArg = Convert.ToInt32(firstString);
+int SecondNumber = 0;
+if (firstArg > 9 && firstArg < 1000)
     {
-        string input = Console.ReadLine();
-        if(!input.Equals("exit"))
-            Console.WriteLine("Вторая цифра числа {0} -> {1}",input, input[1]);
-        else
-            break;
+        SecondNumber = firstArg % 100 / 10;
+        Console.WriteLine("Вторая цифра числа -> ");
+        Console.Write(SecondNumber);
     }
+        else 
+        Console.WriteLine("Число не является трехзначным");
